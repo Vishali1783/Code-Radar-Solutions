4a9f1247555d82c1adcd5480a7906d1aa831812d
Vishali1783 <Vishali1783.becse24@chitkara.edu.in>
2025-01-26 23:35:08 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -6,8 +6,9 @@ int main()
     if(a=='a' | a=='e' | a=='i' | a=='o' | a=='u' | a=='A' | a=='E' | a=='I' | a=='O' | a=='U') {
         printf("Vowel");
     }
-    else{
+    else if(!(a=='a' | a=='e' | a=='i' | a=='o' | a=='u' | a=='A' | a=='E' | a=='I' | a=='O' | a=='U')){
         printf("Consonant");
     }
+   
     return 0;
 }
