884dcab83673cf98cb1494a619dda7935793ab0e
Vishali1783 <Vishali1783.becse24@chitkara.edu.in>
2025-01-31 01:31:59 +0530

    Solution to Print an Increasing Numeric Pyramid

--- Print an Increasing Numeric Pyramid@@67877e1818d53eb724f435d1/code.c
+++ Print an Increasing Numeric Pyramid@@67877e1818d53eb724f435d1/code.c
@@ -8,7 +8,7 @@ int main()
            printf(" ");
         }
         while(k!=2*i - 1){
-            printf("%d",i);
+            printf("%d",j);
             ++k;
         }
         printf("\n");
